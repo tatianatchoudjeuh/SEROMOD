b69bbfdd6a6acc364727db9e0126a485609fa5e0
tatianatchoudjeuh <tngassa.p20@ifi.edu.vn>
2017-02-11 07:09:25 +0100

    suite

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Serological data from hepatite A in Belgium
+#'
+#' A dataset containing the hepatite A status heath of inviduals in Belgium
+#'
+#' @format A data frame with 83 rows and 3 variables:
+#' \describe{
+#'   \item{Id}{individu de l'échantillon,en entier}
+#'   \item{Age}{age des individus de la population,en réel}
+#'   \item{Pos}{nombre de personnes atteintes de la maladie dans la population,en entier}
+#'   \item{Neg}{nombre de personnes saines de la maladie dans la population,en entier}
+#'   \item{Tot}{nombre total de personnes dans la population,en entier}
+#' #' }
+#' @source \url{www.simid.be}
+"HAV_BE"
--- data-raw/HAV-BE.R
+++ data-raw/HAV-BE.R
@@ -1,4 +1,2 @@
-
 HAV_BE<-read.table("HAV-BE.dat",header=T)
-devtools::use_data(HAV_BE)
-save(HAV_BE, file = "data/HAV_BE.RData")
+devtools::use_data( HAV_BE)
